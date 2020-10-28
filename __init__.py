34370f6bbd45e707fed90b8a2ef44d6ccbb0a86e
mathieu <mathieu.hautebas@gmail.com>
2020-10-28 20:46:15 +0100

    prints

--- __init__.py
+++ __init__.py
@@ -17,7 +17,6 @@ def import_iboot(binaryView):
     with open(real_file) as iboot:
         for line in iboot.readlines():
             if "=" in line:
-                print(line.split())
                 iboot_function = line.split()[1]
                 str_addr = line.split()[3]
                 iboot_addr = int(str_addr, 0)
@@ -27,7 +26,7 @@ def import_iboot(binaryView):
         for iboot_function in iboot_functions:
             if function.start == iboot_functions[iboot_function]:
                 function.name = iboot_function
-                print(iboot_function)
+                print(f"[+] {iboot_function}")
 
 
 # Registers the plugin with binja and allows us to specify the function that the binaryview is passed to.
